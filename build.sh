828335235b3d6a4e8c953cf7c6e22c381c57059e
luckyboy <0708yc@gmail.com>
2023-09-20 14:50:07 +0800

    Download extra package without feeds infra

--- build.sh
+++ build.sh
@@ -9,8 +9,25 @@ rm -f .gitkeep
 git config --global advice.detachedHead false
 git clone -b $version_upstream_version_tag --depth 1 https://github.com/openwrt/openwrt.git .
 
-#Merge feeds
-for f in $version_feeds_ ; do eval echo \$${f} >> feeds.conf.default ; done
+# S1 repo url
+download_package(){
+	repo=$(echo $1 | grep -Eo '^http*.*..git')
+	commit=$(echo $1 | grep -Eo '\^.*' | tr -d '^')
+	branch=$(echo $1 | grep -Eo ';.*' | tr -d ';')
+	if [ ! -z $commit ]; then
+		echo Downloading package from $1
+		git -C package clone $repo && cd package/$(basename $repo .git) && git checkout $commit && cd -
+	elif [ ! -z $branch ]; then
+		echo Downloading package from $1
+		git -C package clone $repo --depth 1 --branch $branch
+	else
+		echo "Invalid package info $1"
+		exit 1
+	fi
+}
+
+#Download extra package
+for f in $version_package_ ; do eval download_package \$${f} ; done
 
 #Update feeds
 ./scripts/feeds update -a
@@ -25,10 +42,12 @@ for f in $version_config_ ; do eval echo \$${f} >> .config ; done
 
 #Set dnsmasq-full as default
 sed -i 's/dnsmasq \\/dnsmasq-full \\/g' include/target.mk
+#config luci-theme-argon as default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' .config
+#config version info
 sed -i "s/CONFIG_VERSION_DIST=\"OpenWrt\"/CONFIG_VERSION_DIST=\"OpenWrt-OSM\"/g" .config
 sed -i "s/CONFIG_VERSION_NUMBER=\"\"/CONFIG_VERSION_NUMBER=\"${version_version_number}\"/g" .config
-sed -i "s/CONFIG_VERSION_CODE=\"\"/CONFIG_VERSION_CODE=\"r$(git -C ../ rev-parse --short=10 HEAD)\"/g" .config
+sed -i "s/CONFIG_VERSION_CODE=\"\"/CONFIG_VERSION_CODE=\"$(git -C ../ rev-parse --short=10 HEAD)\"/g" .config
 
 #Download Toolchain
 TOOLCHAIN_PATH=$(find /tmp -maxdepth 1 -name 'toolchain-x86_64_gcc*_musl' -type d)
